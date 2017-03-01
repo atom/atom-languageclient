50c0a021f4477e35be08a9feca7e3b4dc71e05a8
Damien Guard <damieng@gmail.com>
2017-03-01 15:50:15 -0800

    Remove dead files.

---
+++ build/console-logger.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJjb25zb2xlLWxvZ2dlci5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
