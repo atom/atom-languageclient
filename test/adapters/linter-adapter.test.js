6b389e77d1cfa35593f7ac69c967c59ecf916a0e
Damien Guard <damieng@gmail.com>
2017-03-30 12:39:01 -0700

    Add missing test and helper

---
+++ test/adapters/linter-adapter.test.js
@@ -0,0 +1,162 @@
+// @flow
+
+import path from 'path';
+import LinterAdapter from '../../lib/adapters/linter-adapter';
+import * as ls from '../../lib/languageclient';
+import sinon from 'sinon';
+import {expect} from 'chai';
+import Convert from '../../lib/convert';
+import {Point, Range} from 'atom';
+import {createSpyConnection} from '../helpers.js';
+
+describe('LinterAdapter', () => {
+  const defaultLanguageClient = new ls.LanguageClientConnection(createSpyConnection());
+  const compareLinter = (a: linter$Message, b: linter$Message): number => {
+    if (a == null) return 1;
+    if (b == null) return -1;
+    if ((a.filePath || '') < (b.filePath || '')) return -1;
+    if ((a.filePath || '') > (b.filePath || '')) return 1;
+    if ((a.name || '') < (b.name || '')) return -1;
+    if ((a.name || '') > (b.name || '')) return 1;
+    return 0;
+  }
+
+  describe('constructor', () => {
+    it('subscribes to onPublishDiagnostics', () => {
+      const languageClient = new ls.LanguageClientConnection(createSpyConnection());
+      sinon.spy(languageClient, 'onPublishDiagnostics');
+      const linterAdapter = new LinterAdapter(languageClient);
+      expect(languageClient.onPublishDiagnostics.called).equals(true);
+    });
+  });
+
+  describe('captureDiagnostics', () => {
+    const diagnostics = [
+      {
+        range: { start: { line: 1, character: 2 }, end: { line: 3, character: 4 } },
+        severity: ls.DiagnosticSeverity.Error,
+        code: 101,
+        source: 'Unit Test',
+        message: 'Obscure syntax error'
+      },
+      {
+        range: { start: { line: 5, character: 6 }, end: { line: 7, character: 8 } },
+        severity: ls.DiagnosticSeverity.Information,
+        code: 202,
+        source: 'Unit Test 2',
+        message: 'Nice and friendly informational'
+      }
+    ];
+    const makePath = (relative: string) => process.platform === 'win32' ? 'c:' + relative.replace(/\//g, '\\') : relative;
+
+    it('captures diagnostics per path', () => {
+      const linterAdapter = new LinterAdapter(defaultLanguageClient);
+      const filePath = makePath('/a/b.txt');
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(filePath), diagnostics: diagnostics });
+      const results = linterAdapter.provideDiagnostics();
+      expect(results.length).equals(2);
+      results.sort(compareLinter);
+      expect(results[0].filePath).equals(filePath);
+      expect(results[0].name).equals('Unit Test');
+      expect(results[1].severity).equals('info');
+    });
+
+    it('clears previous diagnostics for a path given an empty array of diagnostics', () => {
+      const linterAdapter = new LinterAdapter(defaultLanguageClient);
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/1.txt')), diagnostics: diagnostics });
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/2.txt')), diagnostics: diagnostics });
+      const firstResults = linterAdapter.provideDiagnostics();
+      expect(firstResults.length).equals(4);
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/2.txt')), diagnostics: [] });
+      const secondResults = linterAdapter.provideDiagnostics();
+      expect(secondResults.length).equals(2);
+    });
+
+    it('replaces previous diagnostics for a path given an empty array of diagnostics', () => {
+      const linterAdapter = new LinterAdapter(defaultLanguageClient);
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/1.txt')), diagnostics: diagnostics });
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/2.txt')), diagnostics: diagnostics });
+      linterAdapter.captureDiagnostics({ uri: Convert.pathToUri(makePath('/a/2.txt')), diagnostics: [
+        {
+          range: { start: { line: 10, character: 11 }, end: { line: 12, character: 13 } },
+          severity: ls.DiagnosticSeverity.Warning,
+          code: 303,
+          source: 'Unit Test 3',
+          message: 'Danger, Will Robinson'
+        }
+      ] });
+      const results = linterAdapter.provideDiagnostics();
+      expect(results.length).equals(3);
+    });
+  });
+
+  describe('diagnosticToMessage', () => {
+    it('converts Diagnostic and path to a linter$Message', () => {
+      const filePath = '/a/b/c/d';
+      const diagnostic: ls.Diagnostic = {
+        message: 'This is a message',
+        range: { start: { line: 1, character: 2 }, end: { line: 3, character: 4 } },
+        source: 'source',
+        code: 'code',
+        severity: ls.DiagnosticSeverity.Information,
+        type: ls.DiagnosticSeverity.Information
+      };
+      const result = LinterAdapter.diagnosticToMessage(filePath, diagnostic);
+      expect(result.type).equals('Information');
+      expect(result.text).equals(diagnostic.message);
+      expect(result.name).equals(diagnostic.source);
+      expect(result.filePath).equals(filePath);
+      expect(result.range).deep.equals(new Range(new Point(1, 2), new Point(3, 4)));
+      expect(result.severity).equals('info');
+    });
+  });
+
+  describe('diagnosticSeverityToType', () => {
+    it('converts DiagnosticSeverity.Error to "Error"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToType(ls.DiagnosticSeverity.Error);
+      expect(severity).equals('Error');
+    });
+
+    it('converts DiagnosticSeverity.Warning to "Warning"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToType(ls.DiagnosticSeverity.Warning);
+      expect(severity).equals('Warning');
+    });
+
+    it('converts DiagnosticSeverity.Information to "Information"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToType(ls.DiagnosticSeverity.Information);
+      expect(severity).equals('Information');
+    });
+
+    it('converts DiagnosticSeverity.Hint to "Hint"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToType(ls.DiagnosticSeverity.Hint);
+      expect(severity).equals('Hint');
+    });
+
+    it('converts invalid severity to empty string', () => {
+      const severity = LinterAdapter.diagnosticSeverityToType(-1);
+      expect(severity).equals('');
+    });
+  });
+
+  describe('diagnosticSeverityToSeverity', () => {
+    it('converts DiagnosticSeverity.Error to "error"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToSeverity(ls.DiagnosticSeverity.Error);
+      expect(severity).equals('error');
+    });
+
+    it('converts DiagnosticSeverity.Warning to "warning"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToSeverity(ls.DiagnosticSeverity.Warning);
+      expect(severity).equals('warning');
+    });
+
+    it('converts DiagnosticSeverity.Information to "info"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToSeverity(ls.DiagnosticSeverity.Information);
+      expect(severity).equals('info');
+    });
+
+    it('converts DiagnosticSeverity.Hint to "info"', () => {
+      const severity = LinterAdapter.diagnosticSeverityToSeverity(ls.DiagnosticSeverity.Hint);
+      expect(severity).equals('info');
+    });
+  });
+});
---
+++ test/helpers.js
@@ -0,0 +1,26 @@
+// @flow
+
+import sinon from 'sinon';
+import {TextEditor} from 'atom';
+import * as rpc from 'vscode-jsonrpc';
+
+export function createSpyConnection(): rpc.connection {
+  return {
+    listen: sinon.spy(),
+    onError: sinon.spy(),
+    onUnhandledNotification: sinon.spy(),
+    onNotification: sinon.spy(),
+    dispose: sinon.spy(),
+    sendRequest: sinon.spy()
+  };
+};
+
+export function createFakeEditor(path: ?string): TextEditor {
+  const editor = new TextEditor();
+  sinon.stub(editor, 'getSelectedBufferRange');
+  sinon.spy(editor, 'setTextInBufferRange');
+  editor.setTabLength(4);
+  editor.setSoftTabs(true);
+  editor.getBuffer().setPath(path || '/a/b/c/d.js');
+  return editor;
+};
