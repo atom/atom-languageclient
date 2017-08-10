0f3b688361999e6af3e2291e5e07842cae395830
hansonw <hansonw@fb.com>
2017-08-10 13:59:01 -0700

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,24 @@
 module.exports = {
-    "extends": "fbjs-opensource"
+  'extends': [
+    'fbjs-opensource',
+    'prettier',
+  ],
+  'plugins': [
+    'flowtype',
+    'prettier',
+  ],
+  'rules': {
+    'babel/no-await-in-loop': 0,
+    'camelcase': 0,
+    'flowtype/no-weak-types': 0,
+    'no-new': 0,
+    'prettier/prettier': [1, {
+      'bracketSpacing': false,
+      'jsxBracketSameLine': true,
+      'parser': 'flow',
+      'printWidth': 120,
+      'singleQuote': true,
+      'trailingComma': 'all',
+    }],
+  },
 };
