4f59b27bf38855c48a139a2ab6c6d6dd5af4c978
Damien Guard <damieng@gmail.com>
2017-08-15 16:19:07 -0700

    Disable line-ending rule, cries on Windows

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'camelcase': 0,
     'flowtype/no-weak-types': 0,
     'no-new': 0,
+    'linebreak-style': 0,
     'prettier/prettier': [1, {
       'bracketSpacing': false,
       'jsxBracketSameLine': true,
