cda21406061862fe23cf0016dae0591069bf31f4
hansonw <hansonw@fb.com>
2017-10-10 18:25:36 -0700

    Fix lint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'babel/no-await-in-loop': 0,
     'camelcase': 0,
     'flowtype/no-weak-types': 0,
+    'flowtype/object-type-delimiter': 0,
     'no-new': 0,
     'linebreak-style': 0,
     'prettier/prettier': [1, {
