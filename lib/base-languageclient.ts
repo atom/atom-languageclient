bdc5b1cfe0916799b7e31cc20232155030822375
Nikolay Yakimov <root@livid.pp.ru>
2018-04-13 15:02:44 +0300

    Make handleServerStderr protected
    Better have that override-able, otherwise it's somewhat pointless...

--- lib/base-languageclient.ts
+++ lib/base-languageclient.ts
@@ -375,7 +375,7 @@ export default abstract class BaseLanguageClient {
    * Called on language server stderr output.
    * @param stderr a chunk of stderr from a language server instance
    */
-  private handleServerStderr(stderr: string, _projectPath: string) {
+  protected handleServerStderr(stderr: string, _projectPath: string) {
     stderr.split('\n').filter((l) => l).forEach((line) => this.logger.warn(`stderr ${line}`));
   }
 }
